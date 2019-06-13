9641be99cdc4c83aea270f6df6b0222f2e32f4e0
Doha <doha.o.hossen@hotmail.com>
2019-06-13 11:53:09 +0300

    check

--- G13_Client/src/application/UserHomePage.java
+++ G13_Client/src/application/UserHomePage.java
@@ -1,157 +1,74 @@
-/**
- * Sample Skeleton for 'UserHomePage.fxml' Controller Class
- */
 
-package application;
+ /**
+  * Sample Skeleton for 'UserHomePage.fxml' Controller Class
+  */
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
+ package application;
+
+ import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
+ import javafx.scene.control.TextField;
+ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class UserHomePage implements Initializable{
+ public class UserHomePage {
 
-	@FXML // fx:id="image"
-    private ImageView image; // Value injected by FXMLLoader
+     @FXML // fx:id="Search"
+     private Button Search; // Value injected by FXMLLoader
 
-    @FXML // fx:id="ShowPurchase"
-    private Button ShowPurchase; // Value injected by FXMLLoader
+     @FXML // fx:id="ShowPurchase"
+     private Button ShowPurchase; // Value injected by FXMLLoader
 
-    @FXML // fx:id="WhatToSearch"
-    private TextField WhatToSearch; // Value injected by FXMLLoader
+     @FXML // fx:id="WhatToSearch"
+     private TextField WhatToSearch; // Value injected by FXMLLoader
 
-    @FXML // fx:id="LogOut"
-    private Button LogOut; // Value injected by FXMLLoader
+     @FXML // fx:id="LogOut"
+     private Button LogOut; // Value injected by FXMLLoader
 
-    @FXML // fx:id="Search"
-    private Button Search; // Value injected by FXMLLoader
-    
-    @FXML // fx:id="email"
-    private Label email; // Value injected by FXMLLoader
-    
-	 public void setimage(Image im) {
-			image.setImage(im);
-	    }
-	 
-	 public void set(String input)
-	 {
-			email.setText(input);
-	    }
-	 
-    @FXML
-    void LogOut(ActionEvent event) throws IOException {
-       	FXMLLoader loader = new FXMLLoader(getClass().getResource("Homepage.fxml"));
-    			AnchorPane root = (AnchorPane) loader.load();
-    			Controller home = loader.getController();
-    	    	Image im= new Image("images/world-map-background-copy.jpg");
-    			home.setimage(im);
-    			Scene regist = new Scene(root);
-    			Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-    			app_stage.setScene(regist);
-    			app_stage.show();
-    }
+     @FXML // fx:id="PersonalInformation"
+     private Button PersonalInformation; // Value injected by FXMLLoader
 
-    @FXML
-    void Search(ActionEvent event) {
+     @FXML // fx:id="Email"
+     private Text Email; // Value injected by FXMLLoader
 
-    }
+     @FXML
+     void LogOut(ActionEvent event) throws IOException {
+    	 Parent pane= FXMLLoader.load(getClass().getResource("HomePage.fxml"));
+         Scene log=new Scene(pane);
+         Stage app_Stage=(Stage)((Node)event.getSource()).getScene().getWindow();       
+         app_Stage.setScene(log);
+         app_Stage.show();
+     }
 
-    @FXML
-    void ShowPurchase(ActionEvent event) throws IOException {
-       	FXMLLoader loader = new FXMLLoader(getClass().getResource("Homepage.fxml"));
-		AnchorPane root = (AnchorPane) loader.load();
-		Controller home = loader.getController();
-    	Image im= new Image("images/world-map-background-copy.jpg");
-		home.setimage(im);
-		Scene regist = new Scene(root);
-		Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-		app_stage.setScene(regist);
-		app_stage.show();
+     @FXML
+     void PersonalInformationChange(ActionEvent event) throws IOException {
+    	 Parent pane= FXMLLoader.load(getClass().getResource("PersonalInformatiolnPage.fxml"));
+         Scene log=new Scene(pane);
+         Stage app_Stage=(Stage)((Node)event.getSource()).getScene().getWindow();       
+         app_Stage.setScene(log);
+         app_Stage.show();
+     }
 
-    }
+     @FXML
+     void Search(ActionEvent event) {
 
-	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
-		
-	}
+     }
 
-}
+     @FXML
+     void ShowPurchase(ActionEvent event) throws IOException {
+    	 Parent pane= FXMLLoader.load(getClass().getResource("ShowAndPurchase.fxml"));
+         Scene log=new Scene(pane);
+         Stage app_Stage=(Stage)((Node)event.getSource()).getScene().getWindow();       
+         app_Stage.setScene(log);
+         app_Stage.show();
+     }
 
-///**
-// * Sample Skeleton for 'UserHomePage.fxml' Controller Class
-// */
-//
-///**
-// * Sample Skeleton for 'UserHomePage.fxml' Controller Class
-// */
-//
-//package application;
-//
-//import java.io.IOException;
-//import java.net.URL;
-//import java.util.ResourceBundle;
-//
-//import javafx.event.ActionEvent;
-//import javafx.fxml.FXML;
-//import javafx.fxml.FXMLLoader;
-//import javafx.scene.Node;
-//import javafx.scene.Parent;
-//import javafx.scene.Scene;
-//import javafx.scene.control.Button;
-//import javafx.scene.control.TextField;
-//import javafx.stage.Stage;
-//import javafx.fxml.*;
-//public class UserHomePage implements Initializable {
-//
-//    @FXML // fx:id="Search"
-//    private Button Search; // Value injected by FXMLLoader
-//
-//    @FXML // fx:id="ShowPurchase"
-//    private Button ShowPurchase; // Value injected by FXMLLoader
-//
-//    @FXML // fx:id="WhatToSearch"
-//    private TextField WhatToSearch; // Value injected by FXMLLoader
-//
-//    @FXML // fx:id="LogOut"
-//    private Button LogOut; // Value injected by FXMLLoader
-//
-//    @FXML
-//    void LogOut(ActionEvent event) {
-//
-//    }
-//
-//    @FXML
-//    void Search(ActionEvent event) {
-//
-//    }
-//
-//    @FXML
-//    void ShowPurchase(ActionEvent event) throws IOException {
-//    	Parent pane= FXMLLoader.load(getClass().getResource("ShowAndPurchase.fxml"));
-//        Scene log=new Scene(pane);
-//        Stage app_Stage=(Stage)((Node)event.getSource()).getScene().getWindow();
-//        app_Stage.setScene(log);
-//        app_Stage.show();
-//    }
-//   
-//	@Override
-//	public void initialize(URL arg0, ResourceBundle arg1) {
-//		// TODO Auto-generated method stub
-//		
-//	}
-//	
-//}
+ }
+
--- G13_Client/src/application/signin.java
+++ G13_Client/src/application/signin.java
@@ -40,31 +40,34 @@ public class signin  implements Initializable{
     public void setimage(Image im) {
 		image.setImage(im);
     }
-    @FXML
+	@FXML
     void SignIn(ActionEvent event) throws IOException {
     	
     String Email=email.getText();
     String pass=password.getText();
     	String message="SignIn,"+email.getText()+","+password.getText();
-    	if (Email == null || pass == null) {
+    	if (Email.equals("")|| pass.equals("")) {
 			JOptionPane.showMessageDialog(null, "One or more files are empty!! ");
 		} 
-    	if (!email.getText().equals("")&&!password.getText().equals("")) {
+    	else {
         Connect.client.handleMessageFromClientUI(message);
-        if("SignFailed".equals(Connect.client.servermsg))
-        {
-			JOptionPane.showMessageDialog(null, "You are connected from another device !!");
-
-        }
-        else if ("NotFoundEmail".equals(Connect.client.servermsg)) {
+//        if(Connect.client.servermsg!=null && "SignFailed".equals(Connect.client.servermsg))
+//        {
+//			JOptionPane.showMessageDialog(null, "You are connected from another device !!");
+//			Connect.client.servermsg=null;
+//
+//        }
+        if (Connect.client.servermsg!=null &&"NotFoundEmail".equals(Connect.client.servermsg)) {
 			JOptionPane.showMessageDialog(null, "You are not registed !!");
+			Connect.client.servermsg=null;
 
 		} 
-        else if ("NotFoundPass".equals(Connect.client.servermsg)) {
+        else if (Connect.client.servermsg!=null &&"NotFoundPass".equals(Connect.client.servermsg)) {
 			JOptionPane.showMessageDialog(null, "You entered uncorrect password !!");
+			Connect.client.servermsg=null;
 
    		} 
-        else if ("SignIn".equals(Connect.client.servermsg)) {
+        else if (Connect.client.servermsg!=null &&"SignIn".equals(Connect.client.servermsg)) {
         	if(email.getText().contains("@map.co.il"))
         	{
         		
@@ -72,24 +75,27 @@ public class signin  implements Initializable{
 					AnchorPane root = (AnchorPane) loader.load();
 					employeeHomePage employee = loader.getController();
 					employee.set(email.getText());
+				 	Image im= new Image("images/background.jpg");
+	        		employee.setimage(im);
 					Scene regist = new Scene(root);
 					Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 					app_stage.setScene(regist);
 					app_stage.show();
+					Connect.client.servermsg=null;
+
         	}
         	else if(email.getText().contains("@mapcd.co.il"))
         	{
              	FXMLLoader loader = new FXMLLoader(getClass().getResource("Mhomepage.fxml"));
         		AnchorPane root = (AnchorPane) loader.load();
         		CDMhomePage employee = loader.getController();
-            	Image im= new Image("images/world-map-background-copy.jpg");
+            	Image im= new Image("images/background.jpg");
         		employee.setimage(im);
         		Scene regist = new Scene(root);
         		Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         		app_stage.setScene(regist);
         		app_stage.show();
-					
-					 
+    			Connect.client.servermsg=null;
         	}
         	else if(email.getText().contains("@mapcdm.co.il"))
         	{
@@ -98,21 +104,27 @@ public class signin  implements Initializable{
              Stage app_Stage=(Stage)((Node)event.getSource()).getScene().getWindow();
              app_Stage.setScene(log);
              app_Stage.show();
+ 			Connect.client.servermsg=null;
         	}
         	else {
         		FXMLLoader loader = new FXMLLoader(getClass().getResource("UserHomePage.fxml"));
 				AnchorPane root = (AnchorPane) loader.load();
 				UserHomePage user = loader.getController();
 				user.set(email.getText());
+				Image im= new Image("images/background.jpg");
+				user.setimage(im);
 				Scene regist = new Scene(root);
 				Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 				app_stage.setScene(regist);
 				app_stage.show();
+				Connect.client.servermsg=null;
+
         	}
         }
+    	}
  
     	}
-    }
+    
     	
         
     
@@ -121,22 +133,13 @@ public class signin  implements Initializable{
        	FXMLLoader loader = new FXMLLoader(getClass().getResource("Homepage.fxml"));
     			AnchorPane root = (AnchorPane) loader.load();
     			Controller home = loader.getController();
-    	    	Image im= new Image("images/world-map-background-copy.jpg");
+    	    	Image im= new Image("images/background.jpg");
     			home.setimage(im);
     			Scene regist = new Scene(root);
     			Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
     			app_stage.setScene(regist);
     			app_stage.show();
-		/*
-		 * Parent pane= FXMLLoader.load(getClass().getResource("Homepage.fxml")); Scene
-		 * log=new Scene(pane); Stage
-		 * app_Stage=(Stage)((Node)event.getSource()).getScene().getWindow();
-		 * app_Stage.setScene(log); app_Stage.show();
-		 */
-
     }
-
-
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
